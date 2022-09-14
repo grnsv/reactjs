92805806fdf5c80b99103bf0fcc90dbdfc28880f
grnsv <grin.sv@gmail.com>
2022-09-15 01:43:57 +0300

    lesson-1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// styles
+import './App.scss';
+
+// components
+import Message from './Message';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Message
+          message="message"
+        />
       </header>
     </div>
   );
