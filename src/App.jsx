2bad82435e35a3be3dec4657dc272a4a5922593b
grnsv <grin.sv@gmail.com>
2022-09-17 03:19:14 +0300

    lesson-2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,68 @@
-import React from 'react';
-
-// styles
-import './App.scss';
+import React, { useEffect, useState } from 'react';
 
 // components
-import Message from './Message';
+import {
+  Button, List, TextField, Typography,
+} from '@mui/material';
+import Message from './Components/Message';
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+
+  const handleSubmit = (e) => {
+    const result = [...messageList];
+    result.push({
+      text: e.target.message.value,
+      author: 'User',
+    });
+    setMessageList(result);
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (
+      messageList.length > 0
+      && messageList[messageList.length - 1].author !== 'Robot'
+    ) {
+      setTimeout(() => {
+        const result = [...messageList];
+        result.push({
+          text: 'Ваше мнение очень важно для нас',
+          author: 'Robot',
+        });
+        setMessageList(result);
+      }, 1500);
+    }
+  }, [messageList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Message
-          message="message"
+    <>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Write message here:
+      </Typography>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          name="message"
+          variant="standard"
         />
-      </header>
-    </div>
+        <Button
+          type="submit"
+          variant="contained"
+        >
+          Отправить
+        </Button>
+      </form>
+      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        {messageList.map((message, index) => (
+          <Message
+            key={`${message.author}${index.toString()}`}
+            message={message}
+          />
+        ))}
+      </List>
+    </>
   );
 }
 
