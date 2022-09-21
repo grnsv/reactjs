df81f67a10942f48f04ade39b3770190646a8d0a
grnsv <grin.sv@gmail.com>
2022-09-22 00:37:13 +0300

    lesson-3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 // components
 import {
-  Button, List, TextField, Typography,
+  Button, createTheme, Grid, List, TextField, ThemeProvider, Typography,
 } from '@mui/material';
 import Message from './Components/Message';
+import ChatList from './Components/ChatList';
 
 function App() {
   const [messageList, setMessageList] = useState([]);
@@ -35,34 +36,72 @@ function App() {
     }
   }, [messageList]);
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#FF9800',
+      },
+      secondary: {
+        main: '#0098FF',
+      },
+    },
+  });
+
   return (
-    <>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Write message here:
-      </Typography>
-      <form
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          name="message"
-          variant="standard"
-        />
-        <Button
-          type="submit"
-          variant="contained"
-        >
-          Отправить
-        </Button>
-      </form>
-      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        {messageList.map((message, index) => (
-          <Message
-            key={`${message.author}${index.toString()}`}
-            message={message}
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <ChatList
+            chatList={[
+              {
+                id: '1',
+                name: 'Name1',
+              },
+              {
+                id: '2',
+                name: 'Name2',
+              },
+              {
+                id: '3',
+                name: 'Name3',
+              },
+            ]}
           />
-        ))}
-      </List>
-    </>
+        </Grid>
+        <Grid item>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Write message here:
+          </Typography>
+          <form
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              name="message"
+              variant="standard"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              style={{
+                backgroundColor: theme.palette.primary.main,
+                borderColor: theme.palette.secondary.main,
+              }}
+            >
+              Отправить
+            </Button>
+          </form>
+          <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {messageList.map((message, index) => (
+              <Message
+                key={`${message.author}${index.toString()}`}
+                message={message}
+              />
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
